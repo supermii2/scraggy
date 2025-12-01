7d920752a57c4f9b595adddc3d1c4c38d693cafb
supermii2 <alvis.ng.business@gmail.com>
2025-12-01 15:13:59 +0800

    Change port and add python version

--- backend/backend.py
+++ backend/backend.py
@@ -63,4 +63,5 @@ def search(query: Query):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("backend:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("backend:app", host="0.0.0.0", port=port, reload=True)
